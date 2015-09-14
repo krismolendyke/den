3d2728b03f865314c7ec632f698ec5a3c4ba4c3b
Kris <krismolendyke+github@gmail.com>
2015-09-14 18:11:29 -0400

    Hack in recording of weather data.

--- den/weather.py
+++ den/weather.py
@@ -1,7 +1,9 @@
 """Den weather functionality."""
 
+import logging
 import os
 
+from influxdb import client as influxdb
 import forecastio
 
 try:
@@ -23,12 +25,34 @@ except KeyError:
     raise KeyError("Please set the environment variable 'DEN_LON'.")
 
 
-def get_current():
-    """Get the current weather data.
+def get_current_data():
+    """Get data prepared for InfluxDB insertion.
 
-    :rtype: A ``ForecastioDataPoint`` object.
+    :rtype: :py:class:`dict`
     :returns: The current weather data.
 
     """
     forecast = forecastio.load_forecast(FORECAST_IO_API_KEY, LAT, LON)
-    return forecast.currently()
+    currently = forecast.currently()
+    current_data = currently.d
+    data = [{"name": "weather", "columns": current_data.keys() or [], "points": [current_data.values()]}]
+    logging.debug("Weather data: %s", data)
+    return data
+
+
+def record(database, port, ssl):
+    """Record current weather data into the database.
+
+    :param str database: The name of the database.
+    :param int port: The port number the database is listening on.
+    :param bool ssl: Whether or not to use SSL to communicate with the database.
+    :rtype: :py:const:`None`
+    :return: When the current weather data has been written to the database.
+
+    """
+    db = influxdb.InfluxDBClient(database=database, port=port, ssl=ssl)
+    db.write_points(get_current_data())
+
+
+if __name__ == "__main__":
+    print get_current_data()
