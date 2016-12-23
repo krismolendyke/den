c4dce96f63dd32888df85a71bf307577e40b9648
Kris <krismolendyke@users.noreply.github.com>
2016-12-22 20:28:28 -0500

    propane - Add test.

--- tests/test_propane.py
+++ tests/test_propane.py
@@ -2,10 +2,24 @@
 
 import unittest
 
+from den import propane
+
+# pylint: disable=missing-docstring
+# pylint: disable=protected-access
+# pylint: disable=too-many-public-methods
+
 
 class PropaneTestCase(unittest.TestCase):
-    def test_name(self):
-        self.assertTrue(False, "Unimplemented test.")
+
+    def test_get_api_url(self):
+        expected = "https://data.tankutility.com/api"
+        actual = propane._get_api_url()
+        self.assertEqual(expected, actual)
+
+        token = "test"
+        expected = "https://data.tankutility.com/api?token=test"
+        actual = propane._get_api_url(token=token)
+        self.assertEqual(expected, actual)
 
 
 if __name__ == "__main__":
